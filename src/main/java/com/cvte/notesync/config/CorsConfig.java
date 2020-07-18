7019e28ac2e9deccd505f7fb2cb6370bbe96b16f
= <2473109110@qq.com>
2020-07-18 11:43:24 +0800

    Solve cors problem

--- src/main/java/com/cvte/notesync/config/CorsConfig.java
+++ src/main/java/com/cvte/notesync/config/CorsConfig.java
@@ -1,53 +1,31 @@
 package com.cvte.notesync.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 
 /**
- * 解决跨域问题
+ * 添加跨域配置
  */
-
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                // 允许任何域名
-                .allowedOrigins("*")
-                // 允许证书
-                .allowCredentials(true)
-                // 任何方法
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                // 允许任何
-                .allowedHeaders("*");
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        return corsConfiguration;
     }
 
-//    @Bean
-//    public CorsFilter corsFilter() {
-//
-//        CorsConfiguration config = new CorsConfiguration();
-//        //开放哪些ip、端口、域名的访问权限，星号表示开放所有域
-//        config.addAllowedOrigin("**");
-//        //是否允许发送Cookie信息
-//        config.setAllowCredentials(true);
-//        //开放哪些Http方法，允许跨域访问
-//        config.addAllowedMethod(HttpMethod.DELETE);
-//        config.addAllowedMethod(HttpMethod.GET);
-//        config.addAllowedMethod(HttpMethod.POST);
-//        config.addAllowedMethod(HttpMethod.PUT);
-//        //允许HTTP请求中的携带哪些Header信息
-//        config.addAllowedHeader("**");
-//        //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
-//        config.addExposedHeader("**");
-//
-//        //添加映射路径，“/**”表示对所有的路径实行全局跨域访问权限的设置
-//        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
-//        configSource.registerCorsConfiguration("/**", config);
-//
-//        return new CorsFilter(configSource);
-//    }
-
-
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
 }
--- src/main/java/com/cvte/notesync/config/WebConfig.java
+++ src/main/java/com/cvte/notesync/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.cvte.notesync.config;
 
 import com.cvte.notesync.interceptor.JwtInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,18 +18,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/note/**")
                 .addPathPatterns("/user/**")
                 .addPathPatterns("/sync/**");
-    }
-
-    /**
-     * 添加跨域支持
-     * @param registry
-     */
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowCredentials(true)
-                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS", "HEAD")
-                .maxAge(3600 * 24);
     }
 }
